44f05513cfd7922e33da8c1f886cf7eb9d070776
hodgesds <hodges.daniel.scott@gmail.com>
2014-03-11 20:17:33 -0500

    updated drawing master

--- streamer_master.py
+++ streamer_master.py
@@ -12,10 +12,10 @@ def hello_world(environ, start_response):
     y = randrange(0,h)
     data =  {'color': 'hsl(0,0%,28%)', 'xcord': x, 'ycord': y, 'name': 'master', 'channel': 'master'}
     while True:
-        # do 15 data points
+        # do 10 data points
         x_trend = 5
         y_trend = 5
-        for _ in xrange(0,15):
+        for _ in xrange(0,10):
             x_dir = randrange(0,10) > x_trend 
             y_dir = randrange(0,10) > y_trend
             if x_dir:
@@ -23,11 +23,13 @@ def hello_world(environ, start_response):
                 x_trend -= 1
             else:
                 x -= randrange(0,10)
+                x_trend += 1
             if y_dir:
                 y += randrange(0,10)
                 y_trend -= 1
             else:
                 y -= randrange(0,10)
+                y_trend += 1
             if x>w or x<0:
                 x = randrange(0,w)
             if y>h or y<0:
