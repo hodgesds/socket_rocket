7e389db16e355270910f524df2eedb722279aca3
hodgesds <hodges.daniel.scott@gmail.com>
2014-03-11 20:10:52 -0500

    template update

--- streamer_master.py
+++ streamer_master.py
@@ -6,28 +6,28 @@ import json
 
 def hello_world(environ, start_response):
     start_response('200 OK', [('Content-Type', 'application/json')])
-    w = 900
+    w = 900 # hard coded... ouch
     h = 400
     x = randrange(0,w)
     y = randrange(0,h)
     data =  {'color': 'hsl(0,0%,28%)', 'xcord': x, 'ycord': y, 'name': 'master', 'channel': 'master'}
     while True:
-        #do 10 data points
+        # do 15 data points
         x_trend = 5
         y_trend = 5
         for _ in xrange(0,15):
             x_dir = randrange(0,10) > x_trend 
             y_dir = randrange(0,10) > y_trend
             if x_dir:
-                x += randrange(0,40)
+                x += randrange(0,10)
                 x_trend -= 1
             else:
-                x -= randrange(0,40)
+                x -= randrange(0,10)
             if y_dir:
-                y += randrange(0,40)
+                y += randrange(0,10)
                 y_trend -= 1
             else:
-                y -= randrange(0,40)
+                y -= randrange(0,10)
             if x>w or x<0:
                 x = randrange(0,w)
             if y>h or y<0:
