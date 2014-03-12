2b68a3e423bc093a0a5986ceb6678ca77869591d
hodgesds <hodges.daniel.scott@gmail.com>
2014-03-11 20:31:57 -0500

    updated drawing master function

--- streamer_master.py
+++ streamer_master.py
@@ -1,5 +1,5 @@
 from gevent import pywsgi
-from random import uniform, randrange
+from random import uniform, randrange, random
 import gevent
 import json
 
@@ -19,16 +19,16 @@ def hello_world(environ, start_response):
             x_dir = randrange(0,10) > x_trend 
             y_dir = randrange(0,10) > y_trend
             if x_dir:
-                x += randrange(0,10)
+                x += randrange(0,15)
                 x_trend -= 1
             else:
-                x -= randrange(0,10)
+                x -= randrange(0,15)
                 x_trend += 1
             if y_dir:
-                y += randrange(0,10)
+                y += randrange(0,15)
                 y_trend -= 1
             else:
-                y -= randrange(0,10)
+                y -= randrange(0,15)
                 y_trend += 1
             if x>w or x<0:
                 x = randrange(0,w)
@@ -37,7 +37,8 @@ def hello_world(environ, start_response):
             data['xcord'] = unicode(x)
             data['ycord'] = unicode(y)
             yield json.dumps(data) + '\n'
-        gevent.sleep(uniform(0,1))
+            gevent.sleep(.1)
+
  
 server = pywsgi.WSGIServer(
     ('', 8080), hello_world)
